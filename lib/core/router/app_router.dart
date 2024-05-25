af632feb1ac615a0868f9ae14258653c89f732f6
Trương GIa Mẫn <21521115@gm.uit.edu.vn>
2024-05-25 13:27:19 +0700

    commited

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -54,7 +54,7 @@ GoRouter goRouter(GoRouterRef ref) {
       ),
       GoRoute(
         path: "/category",
-        builder: (context, state) => CaterogyScreen(),
+        builder: (context, state) => const CategoryScreen(),
         routes: const [
         ],
       ),
